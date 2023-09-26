0230c0c3f7c13afed393f4d05fb60f2ac5e033e2
tesinitsyn <sitiev174@gmail.com>
2023-09-26 14:42:38 +0300

    changes

--- main.cpp
+++ main.cpp
@@ -56,18 +56,19 @@ public:
         }
     }
 
-    void inputAuto(int number){
+    void inputAuto(int number) {
         for (int i = 0; i < size; i++) {
             p[i] = number;
         }
     }
-    static int solution(int arrayX[], int arrayC[]){
+
+    static int solution(int arrayX[], int arrayC[]) {
         int totalSum = 0;
-        for(int i = 0; i < 20; i++){
-            if(arrayX[i] < arrayC[i]){
+        for (int i = 0; i < 20; i++) {
+            if (arrayX[i] < arrayC[i]) {
                 return -1;
-            }else{
-                totalSum = pow(arrayX[i], 2 ) - pow(arrayC[i], 2) + totalSum;
+            } else {
+                totalSum = pow(arrayX[i], 2) - pow(arrayC[i], 2) + totalSum;
             }
         }
         return totalSum;
@@ -76,68 +77,29 @@ public:
 
 
 int main() {
-    int solutionType = 10;
-    while (solutionType != 0) {
-        cout << "Lab 4 solutions: \n";
-        cout << "1. Class like method solution\n";
-        cout << "2. Field access solution\n";
-        cout << "If you want to exit write 0 pls :3\n";
-        cout << "Enter number: ";
-        cin >> solutionType;                           
-        if (solutionType == 0) {
-            cout << "Have a good day, honey!!!";
+
+    vect forTest;
+    int arrayX[20];
+    int arrayC[20];
+    string answer;
+    cout << "Enter data automatically or manually[a/m]?";
+    cin >> answer;
+    if (answer == "a") {
+        for (int i = 0; i < 20; i++) {
+            arrayX[i] = 10;
+            arrayC[i] = 1;
         }
-        if (solutionType == 1) {
-            vect forTest;
-            int arrayX[20];
-            int arrayC[20];
-            string answer;
-            cout << "Enter data automatically or manually[a/m]?";
-            cin >> answer;
-            if (answer == "a") {
-                for (int i = 0; i < 20; i++) {
-                    arrayX[i] = 10;
-                    arrayC[i] = 1;
-                }
-            } else {
-                cout << "Input first array: \n";
-                for (int &i: arrayX) {
-                    cin >> i;
-                }
-                cout << "Input second array: \n";
-                for (int &i: arrayC) {
-                    cin >> i;
-                }
-            }
-            cout << "Solution is ";
-            cout << forTest.solution(arrayX, arrayC);
-            cout << "\n";
+    } else {
+        cout << "Input first array: \n";
+        for (int &i: arrayX) {
+            cin >> i;
         }
-        if (solutionType == 2) {
-            int totalSum = 0;
-            vect arrayX(20);
-            vect arrayC(20);
-            string answer;
-            cout << "Enter data automatically or manually[a/m]?";
-            cin >> answer;
-            if (answer == "a") {
-                arrayX.inputAuto(10);
-                arrayC.inputAuto(1);
-            } else {
-               cout << "Input first array\n";
-               arrayX.input();
-               cout << "Input second array\n";
-               arrayC.input();
-            }                                                                                   
-            for (int i = 0; i < arrayX.get_size(); i++) {
-                if(arrayX.element(i) < arrayC.element(i)) {
-                    cout << "Solution is -1";
-                    break;
-                }else{
-                    totalSum = pow(arrayX.element(i), 2 ) - pow(arrayC.element(i), 2) + totalSum;
-                }
-            }
-            cout << "Solution is " + to_string(totalSum) + "\n";
+        cout << "Input second array: \n";
+        for (int &i: arrayC) {
+            cin >> i;
         }
     }
+    cout << "Solution is ";
+    cout << forTest.solution(arrayX, arrayC);
+    cout << "\n";
 }
